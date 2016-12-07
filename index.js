683c9ead923aabed5f19e1a37babbfc0547e4917
Laith Shadeed <laith.shadeed@gmail.com>
2016-12-08 00:05:02 +0100

    linter fixes

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-var fs = require("fs");
-var moment = require("moment");
-var Handlebars = require("handlebars");
+const fs = require('fs');
+const path = require('path');
+const moment = require('moment');
+const Handlebars = require('handlebars');
 
 function render(resume) {
-	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
-	var template = fs.readFileSync(__dirname + "/resume.template", "utf-8");
+  let css = fs.readFileSync(path.join(__dirname, 'style.css'), 'utf-8');
+  let template = fs.readFileSync(path.join(__dirname, 'resume.template'), 'utf-8');
 
-	// Nicer dates
-	Handlebars.registerHelper('date', function(date) {
-    return moment(date).format('MMM YYYY');
-	});
+  // Nicer dates
+  Handlebars.registerHelper('date', function(date) {
+    return moment(date).format('YYYY');
+  });
 
   Handlebars.registerHelper('spaceToDash', function(str) {
     return str.replace(/\s/g, '-').toLowerCase();
@@ -19,12 +20,12 @@ function render(resume) {
     return (new Buffer(str, 'base64')).toString('utf8');
   });
 
-	return Handlebars.compile(template)({
-		css: css,
-		resume: resume
-	});
+  return Handlebars.compile(template)({
+    css: css,
+    resume: resume
+  });
 }
 
 module.exports = {
-	render: render
+  render: render
 };
